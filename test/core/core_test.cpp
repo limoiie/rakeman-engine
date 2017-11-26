8292983399fa7abe93819c6eee3d42949d5c3de5
limoiie <limo.iie4@gmail.com>
2017-11-26 19:10:05 +0800

    <test>: Add test for term2posting and imap

--- test/core/core_test.cpp
+++ test/core/core_test.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
+#include "core/term2posting.h"
 #include "core/tokenizerimpl.h"
 #include "core/normalizerimpl.h"
 #include "core/doc2term.h"
@@ -49,17 +50,19 @@ namespace {
         return std::make_shared<CNormalizerImpl>();
     }
 
+
+    const std::string doc1 = "想要进入“中国天眼”核心观景台，目睹这座庞大" // NOLINT
+            "的“天文机器”，所有人需要经过比机场还要严格的安检，包括手机、"
+            "相机等在内的电子设备一律寄存，不能随身带入景区，这主要是为了"
+            "保护全球最大最强的射电望远镜免受电磁波的干扰";
+
     /**
      * @brief Create a simple doc which will be used to be termed. And This
      * This doc should be sure that it contains more than 10 tokens since we
      * will check this property in following tests.
      */
-    Doc create_a_simple_doc() {
-        std::string content = "想要进入“中国天眼”核心观景台，目睹这座庞大"
-                "的“天文机器”，所有人需要经过比机场还要严格的安检，包括手机、"
-                "相机等在内的电子设备一律寄存，不能随身带入景区，这主要是为了"
-                "保护全球最大最强的射电望远镜免受电磁波的干扰";
-        return Doc(0, "title", content, "cnn", "http://www.baidu.com");
+    Doc create_a_simple_doc(int id = 0, const std::string& content = doc1) {
+        return Doc(id, "title", content, "cnn", "http://www.baidu.com");
     }
 
     TEST(Doc2TermTest, tokenize) { // NOLINT
@@ -95,7 +98,60 @@ namespace {
 
         // make sure this doc has been termed into more than 10 pieces
         // this is a necessary condition
-//        ASSERT_GT(terms.size(), 10);
+        ASSERT_GT(terms.size(), static_cast<size_t>(10));
+    }
+
+    TEST(Term2PostingTest, term2posting) { // NOLINT
+        string key1 = "免受";
+        string key2 = "灾难";
+        string key3 = "大家好";
+        string key4 = "天灾人祸";
+
+        vector<Term> terms;
+        terms.emplace_back(3, key4);
+        terms.emplace_back(1, key2);
+        terms.emplace_back(1, key2);
+        terms.emplace_back(2, key4);
+        terms.emplace_back(0, key1);
+        terms.emplace_back(1, key1);
+        terms.emplace_back(1, key1);
+        terms.emplace_back(0, key2);
+        terms.emplace_back(0, key2);
+        terms.emplace_back(0, key4);
+        terms.emplace_back(1, key1);
+        terms.emplace_back(1, key3);
+        terms.emplace_back(0, key1);
+        terms.emplace_back(0, key1);
+        terms.emplace_back(1, key3);
+
+        PostingsMap<> postings;
+        CTerm2Posting term2Posting;
+        term2Posting.term2posting(terms, postings);
+
+        ASSERT_EQ(postings[key1].size(), 2);
+        ASSERT_EQ(postings[key2].size(), 2);
+        ASSERT_EQ(postings[key3].size(), 1);
+        ASSERT_EQ(postings[key4].size(), 3);
+
+        ASSERT_EQ(postings[key1].front().doc_id, 0);
+        ASSERT_EQ(postings[key1].front().term_freq, 3);
+        postings[key1].pop_front();
+        ASSERT_EQ(postings[key1].front().doc_id, 1);
+        ASSERT_EQ(postings[key1].front().term_freq, 3);
+
+        ASSERT_EQ(postings[key2].front().doc_id, 0);
+        ASSERT_EQ(postings[key2].front().term_freq, 2);
+        postings[key2].pop_front();
+        ASSERT_EQ(postings[key2].front().doc_id, 1);
+        ASSERT_EQ(postings[key2].front().term_freq, 2);
+
+        ASSERT_EQ(postings[key3].front().doc_id, 1);
+        ASSERT_EQ(postings[key3].front().term_freq, 2);
+        postings[key3].pop_front();
+
+        ASSERT_EQ(postings[key4].size(), 3);
+        ASSERT_EQ(postings[key4].front().doc_id, 0);
+        ASSERT_EQ(postings[key4].front().term_freq, 1);
     }
 
 } // namespace

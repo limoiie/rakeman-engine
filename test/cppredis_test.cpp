f77fbb93f68eb6186eb1c1834f1b3e579389f452
limoiie <limo.iie4@gmail.com>
2017-11-30 21:50:06 +0800

    <test>: Add test about redis

--- test/cppredis_test.cpp
+++ test/cppredis_test.cpp
@@ -1,54 +1,19 @@
-#include <cpp_redis/cpp_redis>
-#include <tacopie/tacopie>
-
-#include <condition_variable>
 #include <iostream>
-#include <mutex>
-#include <csignal>
 
+#include <cpp_redis/cpp_redis>
 
-std::condition_variable should_exit;
+using namespace std;
 
-void
-sigint_handler(int) {
-    should_exit.notify_all();
-}
-
-int
-main() {
+int main() {
     //! Enable logging
     cpp_redis::active_logger = std::make_unique<cpp_redis::logger>();
 
-    cpp_redis::subscriber sub;
-
-
-    sub.connect("127.0.0.1", 6379, [](const std::string& host, std::size_t port, cpp_redis::subscriber::connect_state status) {
-        if (status == cpp_redis::subscriber::connect_state::dropped) {
+    cpp_redis::client client;
+    client.connect("127.0.0.1", 6379, [](const std::string& host, std::size_t port, cpp_redis::client::connect_state status) {
+        if (status == cpp_redis::client::connect_state::dropped) {
             std::cout << "client disconnected from " << host << ":" << port << std::endl;
-            should_exit.notify_all();
         }
     });
-
-    //! authentication if server-server requires it
-    // sub.auth("some_password", [](const cpp_redis::reply& reply) {
-    //   if (reply.is_error()) { std::cerr << "Authentication failed: " << reply.as_string() << std::endl; }
-    //   else {
-    //     std::cout << "successful authentication" << std::endl;
-    //   }
-    // });
-
-    sub.subscribe("some_chan", [](const std::string& chan, const std::string& msg) {
-        std::cout << "MESSAGE " << chan << ": " << msg << std::endl;
-    });
-    sub.psubscribe("*", [](const std::string& chan, const std::string& msg) {
-        std::cout << "MESSAGE " << chan << ": " << msg << std::endl;
-    });
-    sub.commit();
-
-    signal(SIGINT, &sigint_handler);
-    std::mutex mtx;
-    std::unique_lock<std::mutex> l(mtx);
-    should_exit.wait(l);
 
     return 0;
 }
---
+++ test/deport_test.cpp
@@ -0,0 +1,34 @@
+//
+// Created by limo on 11/29/17.
+//
+
+#include "gtest/gtest.h"
+
+#include <common/deport.h>
+#include <common/redisdeport.h>
+
+namespace {
+    using namespace std;
+
+    TEST(DeportTest, connect) { // NOLINT
+        IDeport *deport = new CRedisDeport("127.0.0.1", 6379);
+        ASSERT_TRUE(deport->connect());
+    }
+
+    TEST(DeportTest, storeAndFetchPostings) { // NOLINT
+        PostingsMap pm;
+        pm["word_test"] = {{1, 9, {}}, {7, 101, {}}, {8, 12, {}}, {12, 190, {}}, {15, 230, {}}};
+        pm["word_good"] = {{7, 120, {}}, {8, 123, {}}, {9, 1234, {}}, {12, 10, {}}};
+        pm["word_down"] = {{12, 49, {}}, {16, 23, {}}, {18, 3, {}}};
+
+        IDeport *deport = new CRedisDeport("127.0.0.1", 6379);
+        deport->storePostings(pm);
+
+        PostingsMap pm_fetched;
+        pm_fetched = deport->fetchPostings({"word_test", "word_good", "word_down"});
+
+        ASSERT_EQ(pm, pm_fetched);
+
+        // todo: clear all test data from redis
+    }
+}

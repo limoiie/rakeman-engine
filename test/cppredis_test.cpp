957ab7b77204e10522a4f3033c8b7d3fe0052b3a
limoiie <limo.iie4@gmail.com>
2017-12-01 12:00:40 +0800

    <test>: Add test for Postings ops in redis

--- test/cppredis_test.cpp
+++ test/cppredis_test.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 
-#include <cpp_redis/cpp_redis>
+#include "common/redisdeport.h"
 
 using namespace std;
 
 int main() {
     //! Enable logging
-    cpp_redis::active_logger = std::make_unique<cpp_redis::logger>();
+    CRedisDeport deport("127.0.0.1", 6379);
+    if (deport.connect()) {
+        PostingsMap map;
+        map["term1"] = {{123, 888}, {323, 999}, {7123, 777}};
+        map["term2"] = {{324, 666}, {325, 555}, {7233, 111}};
+        map["term3"] = {{589, 333}, {673, 222}, {7453, 444}};
+//        deport.storePostings(map);
 
-    cpp_redis::client client;
-    client.connect("127.0.0.1", 6379, [](const std::string& host, std::size_t port, cpp_redis::client::connect_state status) {
-        if (status == cpp_redis::client::connect_state::dropped) {
-            std::cout << "client disconnected from " << host << ":" << port << std::endl;
+        std::vector<std::string> terms{"term2", "term1", "term3"};
+        PostingsMap fak;
+        deport.fetchPostings(terms, fak);
+
+        for (auto &term : terms) {
+            cout << "map: " << endl;
+            for (auto node : map[term])
+                cout << node.doc_id << ";" << node.term_freq << " ";
+            cout << endl;
+            cout << "node: " << endl;
+            for (auto node : fak[term])
+                cout << node.doc_id << ";" << node.term_freq << " ";
+            cout << endl;
         }
-    });
-
+    } else {
+        throw runtime_error("failed to connect");
+    }
     return 0;
 }

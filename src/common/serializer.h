a472cda52a09c25f68050fff0ea01f48f6bebabb
limoiie <limo.iie4@gmail.com>
2017-12-09 10:47:16 +0800

    <coding>: Coding in serialize module
    Refactor the call style of serialize function; Add specialize implement
    for std::string; Add test for specialize implement for std::string

--- src/common/serializer.h
+++ src/common/serializer.h
@@ -10,27 +10,108 @@
 
 typedef char char_t;
 
+/**
+ * Implement of Integer family.
+ * The format of serialized: num_of_bytes_used_to_store_val|val_memory_bytes
+ *
+ * @tparam T Integer family: int, size_t, long ...
+ * @param val Integer value going to be serialized
+ * @param bytes Serialized string container
+ * @param offset From where the serialized string should store in container.
+ *               NOTE: This param will be updated to the bottom of container.
+ * @return Serialize status
+ */
 template <typename T>
-size_t serialize(const T &val, std::string &bits, size_t offset) {
-    bits.reserve(offset);
+bool serialize(const T &val, std::string &bytes, size_t &offset) {
+    // is the reserve right here?
+    bytes.reserve(offset);
+
+    // step 1: calculate num of bytes in val and store the num into the first
+    //         byte from the %offset of bytes
     size_t size_of_val = sizeof(val);
     // todo: remove useless 0 in front of int and NOTE the negative impl
-    bits.push_back(static_cast<char_t >(size_of_val));
+    bytes.push_back(static_cast<char_t >(size_of_val));
+
+    // step 2: copy bytes in memory of val into container from the bottom
     for (size_t i = 0; i < size_of_val; ++i) {
-        bits.push_back(*((char_t *)(&val) + i));
+        bytes.push_back(*((char_t *)(&val) + i));
     }
-    return size_of_val + 1;
+
+    // step 3: update %offset
+    offset += size_of_val + 1;
+    return true;
 }
 
+/**
+ * Deserialize the Integer family serialized string.
+ */
 template <typename T>
-size_t deserialize(const std::string &bits, size_t offset, T &val) {
+bool deserialize(const std::string &bytes, size_t &offset, T &val) {
     val = 0;
-    auto size_of_val = static_cast<size_t>(bits[offset]);
+
+    // step 1: num of memory bytes in the first byte of %bytes
+    auto size_of_val = static_cast<size_t>(bytes[offset]);
     ++offset;
+
+    // step 2: copy memory bytes into %val
     for (size_t i = 0; i < size_of_val; ++i) {
-        *((char_t *)(&val) + i) = bits[offset+i];
+        *((char_t *)(&val) + i) = bytes[offset+i];
     }
-    return size_of_val + 1;
+
+    // step 3: update %offset
+    offset += size_of_val;
+    return true;
+}
+
+/**
+ * Specialized implement of %std::string.
+ * The format of serialized: serialized_size|std::c_style_string_in_memory
+ *
+ * @param val The value going to be serialized
+ * @param bytes Container used to store the serialized string
+ * @param offset From where the serialized should be store in the container.
+ *               NOTE: This param will be updated to the bottom of container.
+ * @return Serialize status
+ */
+template <>
+bool serialize<std::string>(const std::string &val, std::string &bytes, size_t &offset) {
+    // is the reserve right here?
+    bytes.reserve(offset);
+
+    // step 1: serialize size
+    size_t size_of_val = val.size();
+    serialize(val.size(), bytes, offset);
+
+    // step 2: copy c style string bytes
+    for (size_t i = 0; i < size_of_val; ++i) {
+        bytes.push_back(val[i]);
+    }
+
+    // step 3: update offset
+    offset += size_of_val;
+    return true;
+}
+
+/**
+ * Deserialize from %std::string
+ */
+template <>
+bool deserialize<std::string>(const std::string &bytes, size_t &offset, std::string &val) {
+    val.clear();
+
+    // step 1: deserialize size
+    size_t size_of_val;
+    deserialize(bytes, offset, size_of_val);
+
+    // step 2: copy c style string bytes
+    size_t end_of_val = size_of_val + offset;
+    for (size_t i = offset; i < end_of_val; ++i) {
+        val.push_back(bytes[i]);
+    }
+
+    // step 3: update offset
+    offset += size_of_val;
+    return true;
 }
 
 #endif //RAKEMAN_ENGINE_SERIALIZER_H
--- test/serialize_test.cpp
+++ test/serialize_test.cpp
@@ -14,23 +14,27 @@ namespace {
     TEST(SerializeTest, IntsSerializeTest) { // NOLINT
         std::string str;
         int t, x;
+        size_t o, i;
+        o = 0, i = 0;
 
         x = 38457943;
-        serialize(x, str, 0);
-        deserialize(str, 0, t);
+        serialize(x, str, o);
+        deserialize(str, i, t);
         ASSERT_EQ(t, x);
 
         str.clear();
+        o = 0, i = 0;
 
         x = -38457943;
-        serialize(x, str, 0);
-        deserialize(str, 0, t);
+        serialize(x, str, o);
+        deserialize(str, i, t);
 
         str.clear();
+        o = 0, i = 0;
 
         double z, u = 123.4124;
-        serialize(u, str, 0);
-        deserialize(str, 0, z);
+        serialize(u, str, o);
+        deserialize(str, i, z);
 
         ASSERT_EQ(z, u);
     }
@@ -42,6 +46,24 @@ namespace {
 
         ASSERT_EQ(node.doc_id, fake.doc_id);
         ASSERT_EQ(node.term_freq, fake.term_freq);
+    }
+
+    TEST(SerializeTest, StringSerializeTest) { // NOLINT
+        std::string sample1 = "This is not my fault! You should make sense.";
+        std::string sample2 = "It dose not make any sense! You are a thief.";
+
+        size_t offset = 0;
+        std::string container;
+        serialize(sample1, container, offset);
+        serialize(sample2, container, offset);
+
+        std::string temp;
+
+        offset = 0;
+        deserialize(container, offset, temp);
+        ASSERT_EQ(temp, sample1);
+        deserialize(container, offset, temp);
+        ASSERT_EQ(temp, sample2);
     }
 
 } // namespace
